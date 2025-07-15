502c33e6418e2cb0ef5b7fdb0b280977bbfe0086
anhhuy1806 <huyhero789@gmail.com>
2025-07-15 19:01:51 +0700

    POM

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,24 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+
+    def wait_element(self, locator):
+        """Waits for an element to be present in the DOM."""
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(locator)
+        )
+        return element
+
+    def click(self, locator):
+        """Waits for an element to be clickable and then clicks it."""
+        element = self.wait_element(locator)
+        element.click()
+
+    def send_keys(self, locator, keys):
+        """Waits for an element to be present and sends keys to it."""
+        element = self.wait_element(locator)
+        element.send_keys(keys)
